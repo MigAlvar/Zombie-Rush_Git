b1ceb9d1e4c2862b3c2ed4f91ec82fa4f0bbee7d
Me <migjig.20@gmail.com>
2017-05-19 05:16:44 -0400

    Exercise 4th attempt - Include the coin script

---
+++ Assets/01_Scripts/Coin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Coin : MonoBehaviour{
+
+	
+	public int amount;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.GetComponent<Player>()) {
+			this.gameObject.SetActive(false);
+
+		}	
+	}
+
+	public int coinAmount(){
+		return amount;
+	}
+
+}
